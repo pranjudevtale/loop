415ebe478b637f2b7dc81e37653127c5f0a5375f
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-08-25 11:40:49 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,30 +1,9 @@
-word="python"
+a="python"
 i=0
-while i<len(word):
-    print(" ",end=" ")
+while i<len(a):
     j=0
     while j<=i:
-        print(word[i+j],end=" ")
-        j+=1
-    i+=1
+        print(a[j],end=" ")
+        j=j+1
     print()
-
-# a="python"
-# i=0
-# while i<len(a):
-#     j=0
-#     while j<=i:
-#         print(a[j-i]*i,end=" ")
-#         j+=1
-#     i+=1
-#     print()
-
-
-
-
-
-
-
-
-
-
+    i=i+1
