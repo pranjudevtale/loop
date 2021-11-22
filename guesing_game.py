f3318d616e3caabe64d6364a384338c1cfe41c31
pranjudevtale <84628150+pranjudevtale@users.noreply.github.com>
2021-11-22 09:46:42 +0530

    Add files via upload

---
+++ guesing_game.py
@@ -0,0 +1,29 @@
+# print("you have five chance to guess the numbrer")
+# print("you have enter the number from 1 to 10")
+# i=1
+# while i<=5:
+#     num= int(input("enter the number"))
+#     if num==9:
+#         print("exelent you guess correct number")
+#         break
+#     else:
+#         print("your guess is incorrect try again")
+#     i=i+1
+# else:
+#     print("game over")
+
+# print("you have five chance to guess the numbrer")
+# print("you have enter the number from 1 to 10")
+# i=1
+# while i<=5:
+#     num=int(input("enter the number"))
+#     if num==7:
+#         print("wow,exelent you guess correct number")
+#         break
+#     elif num<7:
+#         print("entered your number is small,try one more time")
+#     elif num>7:
+#         print("entered your number is greater,try one more time")
+#     i+=1
+# else:
+#     print("game over")
